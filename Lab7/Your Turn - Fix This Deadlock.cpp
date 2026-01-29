a97bbd052c80271ab415dc9d9b603c4bb19077f6
tonnamzaza <tonnnamkhung@gmail.com>
2026-01-29 11:01:37 +0700

    Create Your Turn - Fix This Deadlock.cpp

---
+++ Lab7/Your Turn - Fix This Deadlock.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex resourceA, resourceB, resourceC;
+
+void process1()
+{
+    resourceA.lock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    resourceB.lock();
+    // Work...
+    resourceB.unlock();
+    resourceA.unlock();
+}
+
+void process2()
+{
+    resourceB.lock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    resourceC.lock();
+    // Work...
+    resourceC.unlock();
+    resourceB.unlock();
+}
+
+void process3()
+{
+    resourceC.lock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    resourceA.lock();
+    // Work...
+    resourceA.unlock();
+    resourceC.unlock();
+}
+// Solution: Lock all resources in order A -> B -> C
